cfea43cb7d1de1be6a0385d9c42378f97ec94fd4
dueduardoful <dueduardoful@gmail.com>
2020-06-12 16:08:00 -0300

    atributos class projeto

--- src/app/shared/model/projeto.ts
+++ src/app/shared/model/projeto.ts
@@ -1,2 +1,20 @@
+import { Fornecedor } from './fornecedor';
+import { Cliente } from './cliente';
+
 export class Projeto {
+    id: number;
+    nomeProjeto: string;
+    dt_inicio: Date;
+    dt_fim: Date;
+    cep: number;
+    rua: string;
+    cidade: string;
+    bairro: string;
+    numeroCasa: number;
+    complemento: string;
+    valorTotal: number;
+    descricao: string;
+    img: string;
+    fornecedores: Array<Fornecedor>;
+    cliente: Cliente;
 }

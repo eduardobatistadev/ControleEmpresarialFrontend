abc14756d2209cdfb45780dd0f8138a109d647c2
Plops013 <pecorafatec@gmail.com>
2020-06-12 16:05:53 -0300

    Fornecedor model and service

--- src/app/shared/model/fornecedor.ts
+++ src/app/shared/model/fornecedor.ts
@@ -1,2 +1,17 @@
 export class Fornecedor {
+  id?: number;
+  cpf: string;
+  nomeEmpresa: string;
+  atendente?: string;
+  cel: string;
+  tel?: string;
+  email?: string;
+  site?: string;
+  ramoDeAtuacao: string;
+  rua: string;
+  estado: string;
+  cidade: string;
+  bairro: string;
+  numeroCasa: string;
+  obs?: string;
 }
--- src/app/shared/service/fornecedor.service.ts
+++ src/app/shared/service/fornecedor.service.ts
@@ -1,9 +1,36 @@
+import { Fornecedor } from './../model/fornecedor';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FornecedorService {
 
-  constructor() { }
+  url: string;
+
+  constructor(private http: HttpClient) {
+    this.url = 'http://localhost:8080/fornecedor';
+  }
+
+  public save(fornecedor: Fornecedor): Observable<Fornecedor>{
+    return this.http.post<Fornecedor>(this.url, fornecedor);
+  }
+
+  public findAll(): Observable<Fornecedor[]> {
+    return this.http.get<Fornecedor[]>(this.url);
+  }
+
+  public findById(id: number): Observable<Fornecedor> {
+    return this.http.get<Fornecedor>(`${this.url}/${id}`);
+  }
+
+  public deleteById(id: number): void {
+    this.http.delete(`${this.url}/${id}`).subscribe();
+  }
+
+  public update(fornecedor: Fornecedor): Observable<Fornecedor> {
+    return this.http.put<Fornecedor>(this.url, fornecedor);
+  }
 }

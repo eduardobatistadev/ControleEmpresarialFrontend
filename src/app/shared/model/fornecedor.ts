5ec3ad8b65669f31feaba9be608d0af1a0d9e5ff
Plops013 <pecorafatec@gmail.com>
2020-06-12 17:24:54 -0300

    Last update before i go to make dinner

--- src/app/shared/model/fornecedor.ts
+++ src/app/shared/model/fornecedor.ts
@@ -4,12 +4,14 @@ export class Fornecedor {
   nomeEmpresa: string;
   atendente?: string;
   cel: string;
+  cep: string;
   tel?: string;
   email?: string;
   site?: string;
   ramoDeAtuacao: string;
   rua: string;
   estado: string;
+  complemento: string;
   cidade: string;
   bairro: string;
   numeroCasa: string;

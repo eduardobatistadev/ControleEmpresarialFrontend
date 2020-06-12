5cfd2c1350ca49818b4b4176a00c045499f0df25
dueduardoful <dueduardoful@gmail.com>
2020-06-12 16:00:55 -0300

    atributos da class cliente

--- src/app/shared/model/cliente.ts
+++ src/app/shared/model/cliente.ts
@@ -1,2 +1,20 @@
+import { Fornecedor } from './fornecedor';
+import { Projeto } from './projeto';
+
 export class Cliente {
+    id: number;
+    nome: string;
+    cep: number;
+    rua: string;
+    estado: string;
+    bairro: string;
+    numeroCasa: string;
+    complemento: string;
+    dt_nasc: Date;
+    email: string;
+    celular: number;
+    telefoneFixo: number;
+    img: string;
+    fornecedores: Array<Fornecedor>;
+    projetos: Array<Projeto>;
 }

6abf2b8d6f3def07fd9e1440030db687786bd93c
dueduardoful <dueduardoful@gmail.com>
2020-06-15 19:15:44 -0300

    listar cliente implementado!

--- src/app/cliente/listar/listar.component.ts
+++ src/app/cliente/listar/listar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ClienteService } from 'src/app/shared/service/cliente.service';
 import { Cliente } from 'src/app/shared/model/cliente';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listar-cliente',
@@ -9,13 +10,15 @@ import { Cliente } from 'src/app/shared/model/cliente';
 })
 export class ListarComponent implements OnInit {
 
-  cliente: Cliente[] = [];
-  data: Cliente;
+  cliente: Cliente;
+  
 
-  constructor(private clienteService: ClienteService) { }
+  constructor(private clienteService: ClienteService, route: Router) { }
 
   ngOnInit(): void {
-
+    this.clienteService.findAll().subscribe(cliente => {
+      this.cliente = cliente
+    })
   }
 
 }

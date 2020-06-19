09cf7c9a40d14e9711987c4347d68a6e25e63080
Plops013 <pecorafatec@gmail.com>
2020-06-19 15:54:34 -0300

    feat: add Detalhes to route

--- src/app/projeto/projeto-routing.module.ts
+++ src/app/projeto/projeto-routing.module.ts
@@ -1,5 +1,9 @@
+import { DetalhesComponent } from './detalhes/detalhes.component';
 import {HomeComponent} from './home/home.component';
 
 export const ProjetoRouting = [{
     path: 'projeto', component: HomeComponent
-}]
+}, {
+  path: 'projeto/:id', component: DetalhesComponent
+}
+];
